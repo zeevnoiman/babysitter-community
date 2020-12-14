b52e51d97466a489e61171814e11d460792c19ed
zeevnoiman <zevnoiman@gmail.com>
2020-12-14 13:24:48 +0200

    git ignore fixed

--- app-jobs-server/src/config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://zeev:Micalinda10@machonlevproject-qb49v.mongodb.net/jobs?retryWrites=true&w=majority',
-
-    jwtSecret: "0f732e8f60660f1a63cacf025e145712",
-
-    apiGeoKey: '72e8064bb8b841e6a7ed39bb5dd439f5',
-
-    postgresKey: 'micalinda10',
-}
