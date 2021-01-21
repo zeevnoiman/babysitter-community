12757a85f5f61443f889352373cdbae53ec83a91
zeevnoiman <zevnoiman@gmail.com>
2021-01-21 12:04:48 +0200

    refctoring - past works init

--- jobs-mobile/src/pages/PastWorks/index.js
+++ jobs-mobile/src/pages/PastWorks/index.js
@@ -12,10 +12,13 @@ import api from '../../services/api';
 import {staticAddress} from '../../services/api';
 import { userContext } from '../../contexts/UserContext';
 import styles from './styles';
+import { workContext } from '../../contexts/WorkContext';
 
 export default function OpenWorks({navigation}){
 
-    const {loadWorks, works, token} = useContext(userContext);
+    const {user} = useContext(userContext);
+    const {loadWorks, works} = useContext(workContext);
+    
     const [newWorks, setNewWorks] = useState([]);
     const [modalVisible, setModalVisible] = useState(false);
     const [starCount, setStarCount] = useState(0);
@@ -24,28 +27,7 @@ export default function OpenWorks({navigation}){
     const [currentWork, setCurrentWork] = useState(null);
     const [resultMessage, setResultMessage] = useState('');
     var day = []
-    useEffect(() => {
-        loadWorks();
-    }, []);
-
-    useEffect(() => {
-        const newWorks = works.map(async work => {
-        const response = await api.get(`/babysitter/${work.nanny}`,{
-            headers:{
-                authorization : `Bearer ${token}`
-            }
-        });
-
-       // console.log(response.data);
-        
-        work.nannyProfile = response.data;
-        return work;
-    });
-
-    Promise.all(newWorks).then(newWorks =>  setNewWorks(newWorks))
-
-    }, [works])
-
+    
     useEffect(() => {
         if(modalVisible == false){
             if(resultMessage != ''){
@@ -104,135 +86,134 @@ export default function OpenWorks({navigation}){
 
     return (
         <View style={styles.container}>
-            <ImageBackground source={backgroundPattern} style={{height: '100%', width: '100%', position: 'absolute'}}></ImageBackground>
-            
-        <FlatList
-        style={styles.worksList}
-        data={newWorks.sort((a, b) => { 
-                if(isBefore(b.dateHourStartDateFormat, a.dateHourStartDateFormat)){
-                    return -1;
-                }
-                else{
-                    return 1;
-                }
-            })}
-        keyExtractor={ work => String(work._id)}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item : work, index}) => {
-            day = [...day, getDate(work.dateHourStartDateFormat)];
-            
-            return ( 
-                
-                    isBefore(work.dateHourStartDateFormat, new Date()) ?
-                    <View>
-                    {
-                    index != 0 && day[index-1] == getDate(work.dateHourStartDateFormat) ?
-                     null : 
-                     <View style={styles.subtitle}>
-                        <Text  style={styles.subtitleText}>{format(work.dateHourStartDateFormat, 'EEEE') }</Text>
-                        <Text  style={[styles.subtitleText, {marginRight: 5}]}>{format(work.dateHourStartDateFormat, 'dd/MM/yyyy') }</Text>
-                    </View>
+            <ImageBackground source={backgroundPattern} style={{height: '100%', width: '100%', position: 'absolute'}}></ImageBackground>        
+            <FlatList
+            style={styles.worksList}
+            data={newWorks.sort((a, b) => { 
+                    if(isBefore(b.dateHourStartDateFormat, a.dateHourStartDateFormat)){
+                        return -1;
                     }
-                    <View style={[styles.babysitterItem]} >
-                            <View style={styles.avatarContainer}>
-                                { work.nannyProfile.photo.length > 0 ?
-                                <Image style={styles.imageBabysitter} source={{uri : `${staticAddress}${work.nannyProfile.photo}`}}/> 
-                                :
-                                <Image style={styles.imageBabysitter} source={anonimusImage}/>
-                            }
-                            </View>
-                            <View style={styles.babysitterInfo}>
-                                <TouchableOpacity onPress={() => navigation.navigate('SavedNannyProfile', {'worker' : work.nannyProfile})}>
-                                    <Text style={styles.infoText}>{work.nannyProfile.name}</Text>
-                                </TouchableOpacity>
-                                <View style={styles.dateRow}>
-                                <Text style={styles.ageText}>From:</Text>
-                                <Text style={[styles.ageText, {marginLeft: 10}]}>{format(work.dateHourStartDateFormat, 'HH:mm:ss')}</Text>
-                                </View>
-                                <View style={styles.dateRow}>
-                                <Text style={styles.ageText}>To:</Text>                     
-                                <Text style={[styles.ageText, {marginLeft: 10}]}>{work.dateHourFinishReadable}</Text>                     
-                                </View>
-                                <View style={styles.rateBox}>
-                                        <Text style={styles.ageText}>Paid:</Text>
-                                        <FontAwesome name='shekel' size={15} style={[styles.shekel, {marginLeft: 10}]} ></FontAwesome>
-                                        <Text style={styles.ageText}>{work.defined_value_to_pay}/h</Text>    
-                                </View>  
-                            </View>
-                            {work.reviewed == true ? null 
-                            :
-                            <TouchableOpacity style={styles.reviewButton} onPress={() => {
-                                setCurrentWork(work);
-                                setModalVisible(true);    
-                            }}>
-                                <Text  style={styles.ReviewText}>Leave a review</Text>
-                            </TouchableOpacity>}
-                    </View>
-                </View> 
-                :
-                null
-                )}
+                    else{
+                        return 1;
+                    }
+                })}
+            keyExtractor={ work => String(work.id)}
+            showsVerticalScrollIndicator={false}
+            renderItem={({item : work, index}) => {
+                day = [...day, getDate(work.dateHourStartDateFormat)];
+                
+                return ( 
+                    
+                        isBefore(work.dateHourStartDateFormat, new Date()) ?
+                        <View>
+                        {
+                        index != 0 && day[index-1] == getDate(work.dateHourStartDateFormat) ?
+                        null : 
+                        <View style={styles.subtitle}>
+                            <Text  style={styles.subtitleText}>{format(work.dateHourStartDateFormat, 'EEEE') }</Text>
+                            <Text  style={[styles.subtitleText, {marginRight: 5}]}>{format(work.dateHourStartDateFormat, 'dd/MM/yyyy') }</Text>
+                        </View>
                         }
-        />
-        {currentWork &&
-        <Modal
-        animationType='none'
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-        }}
-          >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.titleModal}>Review to {currentWork.nannyProfile.name} </Text>
-            <View style={styles.modalActions}>
-                <Text style={styles.ageText}>Give some stars!</Text>
-                <StarRating
-                    disabled={false}
-                    containerStyle={styles.starsContainer}
-                    starSize={25}
-                    maxStars={5}
-                    rating={starCount}
-                    fullStarColor={'#fff000'}
-                    selectedStar={(rating) => setStarCount(rating)}
-                />
-                <TextInput
-                    style={styles.reviewInput}
-                    placeholder='Tell how was the experience, how was the nanny, if the chidlren were happy...'
-                    placeholderTextColor='#333'
-                    multiline={true}
-                    maxLength={200}
-                    numberOfLines={7}
-                    value={reviewText}
-                    onChangeText={text => setReviewText(text)}>
-                </TextInput>
-                {
-                    reviewRequiredShow &&
-                    <Text style={styles.validationMessage}>Required</Text>
-                }
-            </View>
-                  
-                    <TouchableOpacity
-                      style={styles.modalConfirmButton}
-                      onPress={
-                        handleSubmit}
-                    >
-                      <Text style={styles.confirmText}>Confirm</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      style={styles.modalCloseButton}
-                      onPress={() => {
-                        setReviewRequiredShow(false);
-                        setReviewText('');
-                        setStarCount(0);                    
-                        setModalVisible(false);
-                      }}
-                    >
-                      <EvilIcons name='close' size={20} style={styles.textStyle}></EvilIcons>
-                    </TouchableOpacity>
-                  </View>
+                        <View style={[styles.babysitterItem]} >
+                                <View style={styles.avatarContainer}>
+                                    { work.nannyProfile.photo.length > 0 ?
+                                    <Image style={styles.imageBabysitter} source={{uri : `${staticAddress}${work.nannyProfile.photo}`}}/> 
+                                    :
+                                    <Image style={styles.imageBabysitter} source={anonimusImage}/>
+                                }
+                                </View>
+                                <View style={styles.babysitterInfo}>
+                                    <TouchableOpacity onPress={() => navigation.navigate('SavedNannyProfile', {'worker' : work.nannyProfile})}>
+                                        <Text style={styles.infoText}>{work.nannyProfile.name}</Text>
+                                    </TouchableOpacity>
+                                    <View style={styles.dateRow}>
+                                    <Text style={styles.ageText}>From:</Text>
+                                    <Text style={[styles.ageText, {marginLeft: 10}]}>{format(work.dateHourStartDateFormat, 'HH:mm:ss')}</Text>
+                                    </View>
+                                    <View style={styles.dateRow}>
+                                    <Text style={styles.ageText}>To:</Text>                     
+                                    <Text style={[styles.ageText, {marginLeft: 10}]}>{work.dateHourFinishReadable}</Text>                     
+                                    </View>
+                                    <View style={styles.rateBox}>
+                                            <Text style={styles.ageText}>Paid:</Text>
+                                            <FontAwesome name='shekel' size={15} style={[styles.shekel, {marginLeft: 10}]} ></FontAwesome>
+                                            <Text style={styles.ageText}>{work.defined_value_to_pay}/h</Text>    
+                                    </View>  
+                                </View>
+                                {work.reviewed == true ? null 
+                                :
+                                <TouchableOpacity style={styles.reviewButton} onPress={() => {
+                                    setCurrentWork(work);
+                                    setModalVisible(true);    
+                                }}>
+                                    <Text  style={styles.ReviewText}>Leave a review</Text>
+                                </TouchableOpacity>}
+                        </View>
+                    </View> 
+                    :
+                    null
+                    )}
+                            }
+            />
+            {currentWork &&
+            <Modal
+            animationType='none'
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+            }}
+            >
+            <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                <Text style={styles.titleModal}>Review to {currentWork.nannyProfile.name} </Text>
+                <View style={styles.modalActions}>
+                    <Text style={styles.ageText}>Give some stars!</Text>
+                    <StarRating
+                        disabled={false}
+                        containerStyle={styles.starsContainer}
+                        starSize={25}
+                        maxStars={5}
+                        rating={starCount}
+                        fullStarColor={'#fff000'}
+                        selectedStar={(rating) => setStarCount(rating)}
+                    />
+                    <TextInput
+                        style={styles.reviewInput}
+                        placeholder='Tell how was the experience, how was the nanny, if the chidlren were happy...'
+                        placeholderTextColor='#333'
+                        multiline={true}
+                        maxLength={200}
+                        numberOfLines={7}
+                        value={reviewText}
+                        onChangeText={text => setReviewText(text)}>
+                    </TextInput>
+                    {
+                        reviewRequiredShow &&
+                        <Text style={styles.validationMessage}>Required</Text>
+                    }
                 </View>
-          </Modal>}
+                    
+                        <TouchableOpacity
+                        style={styles.modalConfirmButton}
+                        onPress={
+                            handleSubmit}
+                        >
+                        <Text style={styles.confirmText}>Confirm</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                        style={styles.modalCloseButton}
+                        onPress={() => {
+                            setReviewRequiredShow(false);
+                            setReviewText('');
+                            setStarCount(0);                    
+                            setModalVisible(false);
+                        }}
+                        >
+                        <EvilIcons name='close' size={20} style={styles.textStyle}></EvilIcons>
+                        </TouchableOpacity>
+                    </View>
+                    </View>
+            </Modal>}
         </View>
     )
 }
