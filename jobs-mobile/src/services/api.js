e083dd51bda4b94a67e7393bb6d22ca39562b690
zeevnoiman <zevnoiman@gmail.com>
2021-01-26 18:06:34 +0200

    update url to heroku

--- jobs-mobile/src/services/api.js
+++ jobs-mobile/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.0.0.4:3333',
+    // baseURL: 'http://10.0.0.4:3333',
+    baseURL: 'https://thawing-river-38619.herokuapp.com',
 });
 
-export const staticAddress = 'http://10.0.0.4:3333/static/'
+// export const staticAddress = 'http://10.0.0.4:3333/static/'
+export const staticAddress = 'https://thawing-river-38619.herokuapp.com/static/'
 export default api;
